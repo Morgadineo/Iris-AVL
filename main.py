315761378470087f09e3a0a268cab3abdc06d0f0
rebecabravim <126115022+rebecabravim@users.noreply.github.com>
2025-10-21 15:12:30 -0300

    teste commit

--- main.py
+++
@@ -1,4 +0,0 @@
-from util import avl_tree
-
-if __name__ == "__main__":
-    print("foobar!")
--- util/node.py
+++
@@ -1,5 +0,0 @@
-class Node:
-    def __init__(self, data) -> None:
-        self.left = None
-        self.data = data
-        self.right = None
